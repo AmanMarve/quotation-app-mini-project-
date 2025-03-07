9e025f5f8b37bc798ab3496386cecf376fc076c0
Aman <aman1.marve@gmail.com>
2025-03-07 10:21:00 +0530

    check1 freshed

--- src/components/QuotationPreview.jsx
+++ src/components/QuotationPreview.jsx
@@ -3,8 +3,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 import { toast } from "react-hot-toast";
-import QuotationHeader from "./QuotationHeader";
-import QuotationFooter from "./QuotationFooter";
 
 const QuotationPreview = () => {
   const navigate = useNavigate();
@@ -45,10 +43,40 @@ const QuotationPreview = () => {
       id="quotation-wrapper"
       className="p-6 border shadow-lg max-w-2xl mx-auto"
     >
-      <div id="quotation-format" className="bg-white border border-black">
-        <QuotationHeader />
+      <div
+        id="quotation-format"
+        className="bg-white min-h-auto  border border-black"
+      >
+        <div className="border-b border-black">
+          <div className="flex flex-row sm-p-0 px-2 py-1 sm:flex-row justify-between  font-bold text-[8px] sm:text-base mb-2">
+            <p>GST NO: GST1234567890</p>
+            <p>Contact: 9049384729</p>
+          </div>
+
+          <div className="flex flex-row -mt-5 mb-4 sm:flex-row justify-center items-center gap-1 sm:gap-2">
+            <h1 className="text-3xl sm:text-5xl font-bold">Advik</h1>
+            <h3 className="text-2xl sm:text-4xl font-semibold italic mt-1 sm:mt-2">
+              Enterprises
+            </h3>
+          </div>
+
+          <p className="text-center mb-2 text-sm sm:text-xl italic -mt-4 -sm:mt-1">
+            Sales & Services
+          </p>
+          <div className="italic font-bold p-2 border-black text-center text-[10px] sm:text-base border-t">
+            <p className="-mt-3">Amar chowk, Babupeth ward no.1, Chandrapur</p>
+          </div>
+
+          <div className="border-t italic p-1 font-bold text-[8px] sm:text-[14px] border-black text-center">
+            <p className="-mt-2">
+              Our services: All types of RO system Repairing & Maintenance, All
+              types of Air Conditioning, Refrigerators, Water Coolers & Chiller
+              Plant Repairing & Maintenance
+            </p>
+          </div>
+        </div>
         <div>
-          <div className="flex sm:text-sm text-[9px] font-semibold justify-between pt-1 px-3">
+          <div className="flex sm:text-sm text-[9px] font-semibold justify-between px-3">
             <p>Bill No:</p>
             <p className="italic">
               Date: {new Date().toLocaleDateString("en-GB")}
@@ -56,20 +84,23 @@ const QuotationPreview = () => {
           </div>
           <div className="flex sm:text-lg text-[9px] items-center gap-2 px-3 pb-1">
             M/s. :
-            <p className="text-sm sm:text-lg">{quotation.customer.name}</p>
+            <p className="text-[10px] font-semibold italic sm:text-lg">
+              {quotation.customer.name}
+            </p>
           </div>
-          <table className="w-full text-center">
+
+          <table className="w-full">
             <thead>
-              <tr className="sm:text-[13px] text-[10px] italic">
-                <th className="border-b border-t border-black p-2">
-                  Particulars{" "}
+              <tr className="sm:text-[13px] justify-center items-center text-[10px] italic">
+                <th className="-mt-1 border-b border-t border-black ">
+                  Particulars
                 </th>
-                <th className="border border-black p-2">QTY</th>
-                <th className="border border-black p-2">Rate</th>
-                <th className="border-b border-t border-black p-2">Amount</th>
+                <th className="-mt-1 border border-black">QTY</th>
+                <th className="-mt-1 border border-black ">Rate</th>
+                <th className="-mt-1 border-b border-t border-black">Amount</th>
               </tr>
             </thead>
-            <tbody className="min-h-[500px] sm:text-[13px] text-[10px]">
+            <tbody className="min-h-[500px] text-center sm:text-[13px] text-[10px]">
               {quotation.items.map((item, index) => (
                 <tr key={index} className="gap-0">
                   <td className="border-r border-black p-2">{item.name}</td>
@@ -108,7 +139,13 @@ const QuotationPreview = () => {
             )}
           </h2>
         </div>
-        <QuotationFooter />
+        <div className="flex sm:text-[18px] m-0 text-[12px] font-semibold justify-between items-center p-4 border-t border-black">
+          <p>Thank You...!</p>
+          <div className="flex sm:text-[18px] text-[12px] flex-col items-center justify-between gap-3 sm:gap-5">
+            <p>Advik Enterprises</p>
+            <p>Prop. Ganesh B. Derkar</p>
+          </div>
+        </div>
       </div>
 
       <div className="mt-4 flex space-x-4">
